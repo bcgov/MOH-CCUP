2b2fbdcafd39f2f8e4f955a8972eaa60c889fc26
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-08-14 15:01:32 -0700

    add cypress testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
+import pluginCypress from "eslint-plugin-cypress/flat";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,vue}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  pluginCypress.configs.globals, //eslint formatting for Cypress syntax
+  {
+    files: ["**/*.{js,ts,jsx,tsx,mjs,cjs,vue}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    ignores: ["**/coverage/*", "**/dist/*", "**/.husky/"],
+  },
 ];
