b64870abc401c30d2dc6b4e4c7f1e61fa7976acc
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-08-15 08:15:31 -0700

    add vue router to project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
+import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home </RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
   <div>
     <a
       href="https://vitejs.dev"
@@ -26,6 +33,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <RouterView />
 </template>
 
 <style scoped>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div>
+      <h1>This is a home page</h1>
+    </div>
+  </main>
+</template>
