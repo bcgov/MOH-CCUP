e8810f1d1d6ff2d04e237f1a00219515ffb2f06f
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-09-10 14:27:01 -0700

    add route for get-started page

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/get-started",
+      name: "GetStarted",
+      component: () => import("../views/GetStarted.vue"),
+    },
   ],
 });
 
