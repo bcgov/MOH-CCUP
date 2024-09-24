0263f10bb23e77e66b871dfee02468f9760d8a01
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-09-24 13:36:17 -0700

    add render tests for GetStarted and PractitionerInfo pages

---
+++ tests/unit/PractitionerInfo.spec.js
@@ -0,0 +1,43 @@
+import Page from "@/views/PractitionerInfo.vue";
+import { shallowMount } from "@vue/test-utils";
+import { it, describe, expect } from "vitest";
+import { createRouter, createWebHistory } from "vue-router";
+//beforeEach, afterEach, vi
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
+
+describe("PractitionerInfo.vue", async () => {
+  router.push("/");
+
+  // After this line, router is ready
+  await router.isReady();
+
+  let wrapper = null;
+  // let store = null;
+
+  // beforeEach(async () => {
+  //   let tempForm = cloneDeep(formTemplate);
+  //   tempForm.replaceState(dummyData);
+  //   store = createStore(tempForm);
+
+  wrapper = shallowMount(Page, {
+    global: {
+      plugins: [
+        // store,
+        router,
+      ],
+    },
+  });
+  // });
+
+  // afterEach(() => {
+  //   vi.restoreAllMocks();
+  // });
+
+  it("renders", () => {
+    expect(wrapper.element).toBeDefined();
+  });
+});
