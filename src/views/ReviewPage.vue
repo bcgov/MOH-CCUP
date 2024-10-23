aa635f93475a3d2c657c3eeea01c202255f5929e
Isaac Stevens <Isaac.stevens@pbcsolutions.ca>
2024-10-22 17:24:26 -0700

    merged with main

--- src/views/ReviewPage.vue
+++ src/views/ReviewPage.vue
@@ -11,104 +11,6 @@
         <hr class="mt-2" />
         <div class="m-0 row">
           <div class="col-sm-6">
-            <div class="card border-0">
-              <div class="card-header fw-bold px-0 py-0">
-                <div class="row">
-                  <div class="col-8">Patient information</div>
-                  <div class="col-4 icon-align-right">
-                    <a
-                      href="javascript:void(0)"
-                      class="link-icon"
-                      @click.prevent="Edit(routes.PATIENT_INFO.path)"
-                      >edit&nbsp;&nbsp;<IconPencil
-                        class="icon-style"
-                        color="#1a5a96"
-                    /></a>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body bg-gray">
-                <div class="row">
-                  <h5 class="row-title">Document Type</h5>
-
-                  <div class="row h-100">
-                    <div class="col-6 pt-2">
-                      <span class="px-2 fs-5">Document</span>
-                    </div>
-                    <div class="col-6">
-                      <RadioComponent
-                        id="documents-category"
-                        v-model="documentsCategory"
-                        class="fs-5"
-                        aria-labelledby="documentsCategory"
-                        name="documents-category"
-                        :required="true"
-                        :items="radioOptionsDocumentsCategory"
-                        cypress-id="documents-category"
-                        :disabled="true"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <h5 class="row-title">Adjudicator</h5>
-                  <div class="row row-details">
-                    <div class="col-6">
-                      <span class="fs-5 px-2">First name</span>
-                    </div>
-                    <div class="col-6">
-                      <span class="fs-5">Jane</span>
-                    </div>
-                  </div>
-                  <div class="row row-details">
-                    <div class="col-6">
-                      <span class="fs-5 px-2">Last name</span>
-                    </div>
-                    <div class="col-6">
-                      <span class="fs-5">Doe</span>
-                    </div>
-                  </div>
-                </div>
-                <div class="row mt-3 mb-3">
-                  <h5 class="row-title">Patient</h5>
-                  <div class="row row-details">
-                    <div class="col-6">
-                      <span class="fs-5 px-2">First initial</span>
-                    </div>
-                    <div class="col-6">
-                      <span class="fs-5">S</span>
-                    </div>
-                  </div>
-                  <div class="row row-details">
-                    <div class="col-6">
-                      <span class="fs-5 px-2">Last name</span>
-                    </div>
-                    <div class="col-6">
-                      <span class="fs-5">Jones</span>
-                    </div>
-                  </div>
-                  <div class="row row-details">
-                    <div class="col-6">
-                      <span class="fs-5 px-2">Birthdate</span>
-                    </div>
-                    <div class="col-6">
-                      <span class="fs-5">September 7, 1979</span>
-                    </div>
-                  </div>
-                  <div class="row row-details">
-                    <div class="col-6">
-                      <p class="fs-5 px-2">PHN (Personal Health Number)</p>
-                    </div>
-                    <div class="col-6">
-                      <span class="fs-5">9999999998</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-sm-6">
             <div class="row h-50">
               <div class="card border-0">
                 <div class="card-header fw-bold px-0 py-0">
@@ -130,7 +32,7 @@
                 <div class="card-body bg-gray">
                   <div class="row row-details">
                     <div class="col-6">
-                      <span class="fs-5">First name</span>
+                      <span class="fs-5 fw-bold">First name</span>
                     </div>
                     <div class="col-6 mb-3">
                       <span class="fs-5">Jane</span>
@@ -138,15 +40,15 @@
                   </div>
                   <div class="row row-details">
                     <div class="col-6">
-                      <span class="fs-5">Last name</span>
+                      <span class="fs-5 fw-bold">Last name</span>
                     </div>
                     <div class="mb-3 col-6">
                       <span class="fs-5">Doe</span>
                     </div>
                   </div>
-                  <div class="row row-details">
+                  <div class="mb-0 row">
                     <div class="col-6">
-                      <span class="fs-5">Practitioner number</span>
+                      <p class="mb-0 fs-5 fw-bold">Practitioner number</p>
                     </div>
                     <div class="col-6">
                       <span class="fs-5">A8765</span>
@@ -154,7 +56,7 @@
                   </div>
                   <div class="row row-details">
                     <div class="col-6">
-                      <span class="fs-5">Payee number</span>
+                      <span class="fs-5 fw-bold">Payee number</span>
                     </div>
                     <div class="col-6">
                       <span class="fs-5">87546</span>
@@ -184,7 +86,7 @@
                   <div>
                     <div class="row">
                       <div class="col-6">
-                        <span class="fs-5">Document uploaded</span>
+                        <span class="fs-5 fw-bold">Document uploaded</span>
                       </div>
                       <div class="col-6">
                         <span class="fs-5">1</span>
@@ -192,13 +94,112 @@
                     </div>
                     <div class="row">
                       <div class="col-6">
-                        <span class="fs-5">Note</span>
+                        <span class="fs-5 fw-bold">Note</span>
                       </div>
                       <div class="col-6 col-6 fs-5">
                         <p>
                           Included is a description document of the procedure performed by the Dr.
                         </p>
                       </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card border-0">
+              <div class="card-header fw-bold px-0 py-0">
+                <div class="row">
+                  <div class="col-8">Patient information</div>
+                  <div class="col-4 icon-align-right">
+                    <a
+                      href="javascript:void(0)"
+                      class="link-icon"
+                      @click.prevent="Edit(routes.PATIENT_INFO.path)"
+                      >edit&nbsp;&nbsp;<IconPencil
+                        class="icon-style"
+                        color="#1a5a96"
+                    /></a>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body bg-gray">
+                <div class="row">
+                  <h5 class="row-title">Document Type</h5>
+
+                  <div class="row h-100">
+                    <div class="col-6 pt-2">
+                      <span class="px-2 fs-5 fw-bold">Document</span>
+                    </div>
+                    <div class="col-6">
+                      <RadioComponent
+                        id="documents-category"
+                        v-model="documentsCategory"
+                        class="fs-5"
+                        aria-labelledby="documentsCategory"
+                        name="documents-category"
+                        :required="true"
+                        :items="radioOptionsDocumentsCategory"
+                        cypress-id="documents-category"
+                        :disabled="true"
+                        :model-value="pre - auth"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <h5 class="row-title">Adjudicator</h5>
+                  <div class="row row-details">
+                    <div class="col-6">
+                      <span class="fs-5 px-2 fw-bold">First name</span>
+                    </div>
+                    <div class="col-6">
+                      <span class="fs-5">Alice</span>
+                    </div>
+                  </div>
+                  <div class="row row-details">
+                    <div class="col-6">
+                      <span class="fs-5 px-2 fw-bold">Last name</span>
+                    </div>
+                    <div class="col-6">
+                      <span class="fs-5">Johnson</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="row mt-3 mb-3">
+                  <h5 class="row-title">Patient</h5>
+                  <div class="row row-details">
+                    <div class="col-6">
+                      <span class="fs-5 px-2 fw-bold">First initial</span>
+                    </div>
+                    <div class="col-6">
+                      F
+                      <span class="fs-5"></span>
+                    </div>
+                  </div>
+                  <div class="row row-details">
+                    <div class="col-6">
+                      <span class="fs-5 px-2 fw-bold">Last name</span>
+                    </div>
+                    <div class="col-6">
+                      <span class="fs-5">Martinez</span>
+                    </div>
+                  </div>
+                  <div class="row row-details">
+                    <div class="col-6">
+                      <span class="fs-5 px-2 fw-bold">Birthdate</span>
+                    </div>
+                    <div class="col-6">
+                      <span class="fs-5">February 23, 1985</span>
+                    </div>
+                  </div>
+                  <div class="row row-details">
+                    <div class="col-6">
+                      <p class="fs-5 px-2 fw-bold">PHN (Personal Health Number)</p>
+                    </div>
+                    <div class="col-6">
+                      <span class="fs-5">1234567890</span>
                     </div>
                   </div>
                 </div>
