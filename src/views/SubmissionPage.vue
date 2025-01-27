275167e5a0084b660afcbf0b238d9c4d79f3c9c9
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2025-01-27 09:16:44 -0800

    refactor declarations to one location to prevent copy pasting text

--- src/views/SubmissionPage.vue
+++ src/views/SubmissionPage.vue
@@ -58,17 +58,10 @@
         <h2 class="mt-5 mb-0">Declaration of accuracy and validity</h2>
         <hr class="mt-0" />
         <p>
-          I hereby declare that the information provided through this web form is accurate,
-          complete, and truthful to the best of my knowledge. I confirm that all information
-          provided pertains to a single patient and is valid for a single claim or
-          pre-authorization. I understand that any false, misleading, or omitted information may
-          result in the rejection of my submission and could have legal consequences.
+          {{ declarationAccuracy }}
         </p>
         <p>
-          By submitting this form, I affirm that I have personally completed all sections and that
-          the information I have provided is a true and honest representation of the facts. I
-          acknowledge that it is my responsibility to update any changes to this information
-          promptly.
+          {{ declarationValidity }}
         </p>
         <CheckboxComponent
           id="pratitioner-declaration-accuracy"
@@ -90,6 +83,7 @@ import { routes } from "../router/index.js";
 import { useFormStore } from "@/stores/formData";
 // import { smallStyles, mediumStyles } from "@/constants/input-styles";
 import beforeRouteLeaveHandler from "@/helpers/beforeRouteLeaveHandler.js";
+import { declarationAccuracy, declarationValidity } from "@/constants/declarations.js";
 </script>
 
 <script>
