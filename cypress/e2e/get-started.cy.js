baf468f375fa67741fffdf53b88fbac03c0021a1
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2026-01-13 08:47:22 -0800

    add api intercepts for stability

--- cypress/e2e/get-started.cy.js
+++ cypress/e2e/get-started.cy.js
@@ -1,5 +1,45 @@
+import envData from "../fixtures/env-data.js";
+
 describe("get started", () => {
   it("goes to the get started page when the user visits /", () => {
+    if (envData.enableIntercepts) {
+      console.log("intercepted captcha calls with 200 OK response");
+      cy.intercept("POST", "/ccup/api/captcha/captcha", {
+        statusCode: 200,
+        body: {
+          nonce: "1234567890",
+          captcha: "captcha",
+          testfield: "This is a stubbed test response from Cypress",
+        },
+      });
+
+      cy.intercept("POST", "/ccup/api/captcha/verify/captcha", {
+        statusCode: 200,
+        body: {
+          valid: true,
+          jwt: "1234567890",
+          testfield: "This is a stubbed test response from Cypress",
+        },
+      });
+
+      console.log("intercepted logging calls with 200 OK response");
+      cy.intercept("POST", "/ccup/api/logging", {
+        statusCode: 200,
+        body: {
+          returnCode: "success",
+          testfield: "This is a stubbed test response from Cypress",
+        },
+      });
+
+      console.log("intercepted env calls with 200 OK response");
+      cy.intercept("POST", "/ccup/api/env", {
+        statusCode: 200,
+        body: {
+          returnCode: "success",
+          testfield: "This is a stubbed test response from Cypress",
+        },
+      });
+    }
     cy.visit("/");
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/ccup/");
